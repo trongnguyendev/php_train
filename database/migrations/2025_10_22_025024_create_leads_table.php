fa14813907ee07eaa446a1a3c3e23c4e1d7a67c0
Thanh Ý Phan <mac@inputinput.local>
2025-12-04 09:48:06 +0700

    ip

--- database/migrations/2025_10_22_025024_create_leads_table.php
+++ database/migrations/2025_10_22_025024_create_leads_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('zalo');
             $table->string('customer_type_id');
             $table->string('showroom_id')->nullable();
-            $table->string('product_categories_id');
+            $table->unsignedBigInteger('product_categories_id')->nullable();
             $table->string('first_customer_status_id');
             $table->longtext('note');
             $table->string('sale_information_id');
