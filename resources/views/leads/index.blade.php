ddbac83f700bd28e7cde319eb18cee91d853c7f0
Thanh Ý Phan <mac@inputinput.local>
2025-12-08 12:55:07 +0700

    fix

--- resources/views/leads/index.blade.php
+++ resources/views/leads/index.blade.php
@@ -296,7 +296,7 @@
                                         </td>
                                         <td>
                                             <span class="editable-select" data-id="{{ $lead->id }}" data-field="source_id">
-                                                {{ $lead->customerSource ? $lead->customerSource->name : '-' }}
+                                                {{ $lead->customerSources ? $lead->customerSources->name : '-' }}
                                             </span>
                                             <select class="form-select d-none inline-select" data-id="{{ $lead->id }}" data-field="source_id">
                                                 <option value="">-- Chọn --</option>
