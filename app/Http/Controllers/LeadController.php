b81bf394fde44a252446c1abfda34f7a8fab0e61
Thanh Ý Phan <mac@inputinput.local>
2026-06-22 09:29:41 +0700

    fix hienthi 2

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -194,7 +194,10 @@ public function index(Request $request)
         // Paginate results to improve performance (30 rows per page)
         // $leads = $query->where('lead_type', 1)->latest()->paginate(30, ['*'], 'direct_page');
         $leads = $query
-            ->where('sale_information_id', $user->id)
+            ->when(
+                !in_array($user->position, ['admin', 'manager', 'supporter']),
+                fn ($q) => $q->where('sale_information_id', $user->id)
+            )
             ->latest()
             ->paginate(30, ['*'], 'direct_page');
         $leadsOnline = $queryOnline->where('lead_type', 2)->latest()->paginate(30, ['*'], 'online_page');
