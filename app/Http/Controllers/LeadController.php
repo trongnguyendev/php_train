a71eaee4b361bc36fd34f053f200f5d118682604
Thanh Ý Phan <mac@inputinput.local>
2026-06-22 09:40:53 +0700

    fix hienthi5

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -198,10 +198,15 @@ public function index(Request $request)
             ->exists();
 
         $leads = $query
-            ->when(
-                !$isAdminOrManager,
-                fn ($q) => $q->where('sale_information_id', $user->id)
-            )
+            ->when(!$isAdminOrManager, function ($q) use ($user) {
+                $q->where(function ($sub) use ($user) {
+                    $sub->where('lead_type', 2)
+                        ->orWhere(function ($x) use ($user) {
+                            $x->where('lead_type', 1)
+                            ->where('sale_information_id', $user->id);
+                        });
+                });
+            })
             ->latest()
             ->paginate(30, ['*'], 'direct_page');
         $leadsOnline = $queryOnline->where('lead_type', 2)->latest()->paginate(30, ['*'], 'online_page');
