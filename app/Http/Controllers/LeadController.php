f38fc5b2053fce1819a193f55cac57d27e9e99df
Thanh Ý Phan <mac@inputinput.local>
2026-08-14 21:10:18 +0700

    fix lead 1

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -711,12 +711,13 @@ public function update(Request $request, Lead $lead)
     }
 
     // Chỉ cập nhật bản ghi Lead hiện tại
-    $oldLeadType = $lead->lead_type;
+    $oldLeadType = $lead->old_lead_type ?? $lead->lead_type;
     $newLeadType = $request->lead_type;
 
-    if ($oldLeadType == 1 && $newLeadType == 2) {
+    if ($oldLeadType == 1 && $newLeadType == 2 && $lead->lead_type == 1) {
         $lead->update([
-            'lead_type' => 2
+            'old_lead_type' => $oldLeadType,
+            'lead_type' => 2,
         ]);
     }
     // dd($oldLeadType, $newLeadType);
